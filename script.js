983c8ed6194fadb35f58b94d790aeb353f4c2ded
Jose Ruiz <jruiz793ric@gmail.com>
2024-09-23 20:59:39 -0300

    Color div

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const colorContainer = document.querySelector('.container');
 
 color.addEventListener('input', (event) => {
     const colorHeader = event.target.value;
-
     header.style.background = `linear-gradient(${colorHeader}, #fafafa)`;
 });
+colorContainer.addEventListener('click', (event) => {
+    if(event.target.tagName === 'DIV'){
+        const selectedColor = window.getComputedStyle(event.target).backgroundColor;
+        header.style.background = `linear-gradient(${selectedColor}, #fafafa)`;
+    }
+})
